3b3e3996b9b4eca8146e17cc79c9cc58cffe5a00
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-07-26 16:10:19 -0600

    figuras ahora funciona de forma dinamica usando funciones

--- figurasGeometricas/figuras.js
+++ figurasGeometricas/figuras.js
@@ -1,48 +1,42 @@
 // Cuadrado
 console.group("Cuadrado")
 
-const ladoCuadrado = 5
-console.log(`Los lados del cuadrado miden ${ladoCuadrado} cm`)
+function perimetroCuadrado(lado) {
+    return lado * 4
+}
 
-const perimetroCuadrado = ladoCuadrado * 4
-console.log(`El perimetro del cuadrado es ${perimetroCuadrado} cm`)
-
-const areaCuadrado = ladoCuadrado * ladoCuadrado
-console.log(`El área del cuadrado es ${areaCuadrado} cm²`)
-
+function areaCuadrado(lado){
+    return lado * lado
+}
 console.groupEnd()
 
 // Triángulo
 console.group("Triángulo")
 
-const lado1Triangulo = 6
-const lado2Triangulo = 6
-const baseTriangulo = 4
-const alturaTriangulo = 5.5
+function perimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base
+}
 
-console.log(`Los lados del triángulo miden ${lado1Triangulo}, ${lado2Triangulo} y ${baseTriangulo} cm`)
-
-const perimetroTriangulo = lado1Triangulo + lado2Triangulo + baseTriangulo
-console.log(`El perimetro del cuadrado es ${perimetroTriangulo} cm`)
-
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2
-console.log(`El área del cuadrado es ${areaCuadrado} cm²`)
+function areaTriangulo(base, altura){
+    return (base * altura) / 2
+}
 
 console.groupEnd()
 
 // Círculo
 console.group("Círculo")
 
-const radioCirculo = 6
-const diametroCirculo = radioCirculo * 2
-const pi = Math.PI
-const circunferenciaCirculo = diametroCirculo * pi
-const areaCirculo = (radioCirculo * radioCirculo) * pi
+function diametroCirculo(radio){
+    return radio * 2
+}
 
-console.log(`El diámetro del círculo mide ${radioCirculo} cm`)
+function circunferenciaCirculo(radio){
+    const diametro = diametroCirculo(radio) // Llama a esta función para evitar redundancia de código
+    return diametro * Math.PI
+}
 
-console.log(`PI es ${pi}`)
-
-console.log(`El área del círculo es ${areaCirculo} cm²`)
+function areaCirculo(radio){
+    return (radio * radio) * Math.PI
+}
 
 console.groupEnd()
